1845a22acf33807c084152775d6196050bdb6ac1
subhash1599 <subhashnimmala01@gmail.com>
2025-03-11 10:52:25 +0530

    added efficient approach for leaders array code

--- Arrays/leader_array.py
+++ Arrays/leader_array.py
@@ -9,11 +9,20 @@ def leader_array_brute_force(arr):
             print(arr[i])
         
 
-
+def leader_array_efficient(arr):
+    n=len(arr)
+    curr_ladder=arr[n-1]
+    print(curr_ladder)
+    for i in range(n-2,-1,-1):
+        if(curr_ladder<arr[i]):
+            curr_ladder=arr[i]
+            print(curr_ladder,"curr")
 
 
 if __name__=="__main__":
-    leader_array([7,10,4,3,6,5,2])
+    leader_array_brute_force([7,10,4,3,6,5,2])
+    leader_array_efficient([7,10,4,3,6,5,2])
+
 
 
 
