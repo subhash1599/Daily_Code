9bba86ca2fd90bb7c5e11bc4325430b06376095a
subhash1599 <subhashnimmala01@gmail.com>
2025-03-19 23:59:14 +0530

    efficient code

--- Arrays/maximum_length_even_odd_subarray.py
+++ Arrays/maximum_length_even_odd_subarray.py
@@ -10,7 +10,23 @@ def even_odd_subarray(arr):
         res=max(res,c)
     return res
 
+
+def even_odd_subarray_efficient(arr):
+    res=1
+    c=1
+    for j in range(len(arr)):
+        
+        if (arr[j]%2==0 and arr[j-1]%2!=0) or (arr[j]%2!=0 and arr[j-1]%2==0):
+                c+=1
+                res=max(res,c)
+        else:
+            c=1
+        
+        res=max(res,c)
+    return res
+
 if __name__=="__main__":
     print(even_odd_subarray([10,12,14,7,8]))
     print(even_odd_subarray([5,10,20,6,3,8]))
+    print(even_odd_subarray_efficient([5,10,20,6,3,8]))
 
