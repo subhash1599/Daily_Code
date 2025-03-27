5f3b7638de4ec7bf1ec1378fc1dec7b71f8bdb74
subhash1599 <subhashnimmala01@gmail.com>
2025-03-27 21:48:09 +0530

    naive solution added

---
+++ Arrays/max_appearing_element.py
@@ -0,0 +1,24 @@
+def max_appearing_element(left,right,n):
+    freq=[0]*100
+    for i in range(n):
+        for j in range(left[i],right[i]+1):
+            freq[j]+=1
+    
+    res=0
+    for i in range(1,100):
+        if freq[i]>freq[res]:
+            res=i
+
+    return res
+       
+    
+
+if __name__ == '__main__':
+    left=[1,2,4]
+    right=[4,5,7]
+    n=len(left)
+    print(max_appearing_element(left,right,n))
+    left=[1,2,5,15]
+    right=[5,8,7,18]
+    n=len(left)
+    print(max_appearing_element(left,right,n))
