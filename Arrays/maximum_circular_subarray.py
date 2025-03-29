0b92aa98ab4ecdb20e1e97512f08c90d6109e375
subhash1599 <subhashnimmala01@gmail.com>
2025-03-29 22:37:46 +0530

    Added another approach

--- Arrays/maximum_circular_subarray.py
+++ Arrays/maximum_circular_subarray.py
@@ -9,5 +9,28 @@ def max_circular_subarray(arr):
             curr_max=max(curr_max,curr_sum)
         res=max(res,curr_max)
     return res
+
+
+def normal_max_subarray(arr):
+    res=arr[0]
+    maxEnding=arr[0]
+    for i in range(len(arr)):
+        maxEnding=max(maxEnding+arr[i],arr[i])
+        res=max(maxEnding,res)
+    return res
+
+def overall_max_subarray(arr):
+    max_normal=normal_max_subarray(arr)
+    if(max_normal<0):
+        return max_normal
+    arr_sum=0
+    for i in range(len(arr)):
+        arr_sum+=arr[i]
+        arr[i]-=arr[i]
+    max_circular=arr_sum+normal_max_subarray(arr)
+    return max(max_normal,max_circular)
+
+
 if __name__=="__main__":
     print(max_circular_subarray([5,-2,3,4]))
+    print(overall_max_subarray([8,-4,3,-5,4]))
