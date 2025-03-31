985843d800c86ae16ed24e62c2d19da7c5e8dc64
subhash1599 <subhashnimmala01@gmail.com>
2025-03-31 12:26:05 +0530

    binary search recursive approach

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,4 +1,4 @@
-def binary_seatch(arr,key):
+def binary_search(arr,key):
     low,high=0,len(arr)-1
     while(low<=high):
         mid=(low+high)//2
@@ -10,6 +10,21 @@ def binary_seatch(arr,key):
             return mid
     return -1
 
+
+
+def binary_search_recursive(arr,key,low,high):
+    if(low>high):
+        return -1
+    mid=(low+high)//2
+    if(arr[mid]>key):
+        return binary_search_recursive(arr,key,low,mid-1)
+    elif(arr[mid]<key):
+        return binary_search_recursive(arr,key,mid+1,high)
+    else:
+        return mid
+
+
 if __name__=="__main__":
     arr=[10,20,30,40,50,60,70]
-    print(binary_seatch(arr,60))
+    print(binary_search(arr,60))
+    print(binary_search_recursive(  arr,20,0,len(arr)-1))
