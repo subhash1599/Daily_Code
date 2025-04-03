b05117160f0a53469a4df3fe42de5cf8c7f193ed
subhash1599 <subhashnimmala01@gmail.com>
2025-04-03 22:22:17 +0530

    Added one approach

---
+++ Searching/count_ocurrences.py
@@ -0,0 +1,14 @@
+def count_occurence(arr,key):
+    count=0
+    for i in range(len(arr)):
+        if(arr[i]==key):
+            count+=1
+    return count
+
+if __name__ == "__main__":
+    arr=[1,10,10,10,20,20,40]
+    key=5
+    print(count_occurence(arr,key))
+    arr=[5,10,10,15,20,20,20]
+    key=20
+    print(count_occurence(arr,key))
