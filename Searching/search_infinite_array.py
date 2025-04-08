600a26efe3ab0e85c96debb5e8bd3f47b682e91d
subhash1599 <subhashnimmala01@gmail.com>
2025-04-08 21:43:28 +0530

    Added another approach for the problem

--- Searching/search_infinite_array.py
+++ Searching/search_infinite_array.py
@@ -7,7 +7,25 @@ def infinite_array_search(arr,key):
             return -1
         i+=1
 
+def infinite_array_search_efficient(arr,key):
+    if(arr[0]==key): return key
+    i=1
+    while(arr[i]<key):
+        i*=2
+    low,high=i//2,i
+    while(low<=high):
+        mid=(low+high)//2
+        if(arr[mid]==key):
+            return mid
+        elif(arr[mid]<key):
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+
+
 if __name__ == "__main__":
     print(infinite_array_search([1,10,200],15))
+    print(infinite_array_search_efficient([1,10,15,20,40,60,80,100,200,500,1000],100))
 
     
