ee2d70903ac9d36369f55cefe67cd272a7a594e2
subhash1599 <subhashnimmala01@gmail.com>
2025-04-10 21:37:21 +0530

    added comment

--- Searching/count_ocurrences.py
+++ Searching/count_ocurrences.py
@@ -3,6 +3,7 @@ def count_occurence(arr,key):
     for i in range(len(arr)):
         if(arr[i]==key):
             count+=1
+            #display the count
     return count
 
 if __name__ == "__main__":
